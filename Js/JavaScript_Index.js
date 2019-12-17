b7139348eaf7bfd01fa73837a446c4b3c0267713
Mekkaoui <hanane03.mekkaoui@gmail.com>
2019-12-17 12:19:31 +0100

    Update

--- Js/JavaScript_Index.js
+++ Js/JavaScript_Index.js
@@ -1,15 +1,31 @@
- 
-
+ function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else { 
+  	P1.style.visibility="visible";
+    P1.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+    
+function showPosition(position) {
+ sessionStorage.setItem("latitude", position.coords.latitude);
+ sessionStorage.setItem("longitude", position.coords.longitude);
+   document.location.href="./Acceuil.html";
+}
 function SeConnecter()
-{var login=document.getElementById("userName").value;
+{
+	var login=document.getElementById("userName").value;
 var PassWord=document.getElementById("password").value;
  var P1=document.getElementById("p1");
+
 if(login=="Hanane" && PassWord=="0903")
 {
-        // Store
+        // Store UserName
+  
        sessionStorage.setItem("UserName", "Hanane");
-       document.location.href="./Acceuil.html"; 
-     
+        getLocation();
+
+       
 
 }
 else{ 
