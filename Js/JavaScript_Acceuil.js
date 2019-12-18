e16e2af0ff082fc198b181694098aac9d8287bb0
hanane <hanane03.mekkaoui@gmail.com>
2019-12-18 22:27:27 +0100

    update

--- Js/JavaScript_Acceuil.js
+++ Js/JavaScript_Acceuil.js
@@ -1,8 +1,58 @@
 function Deconnecter()
 {    var Dc=document.getElementById("dec");
 	sessionStorage.clear();
-	dec.setAttribut("href","./Index.html");
+	Dc.href='./Index.html';
+	//Dc.setAttribut("href","./Index.html");
   // document.location.href="./Index.html"; 
 
 
 }
+
+const weathericon={
+
+	"Rain":"wi wi-day-rain",
+	"Clouds":"wi wi-day-cloudy",
+	"clear":"wi wi-day-sunny",
+	"Snow":"wi wi-day-snow",
+	"mist":"wi wi-day-fog",
+	"Drizzle":"wi wi-day-sleet",
+}
+
+function capital(chaine){
+  return chaine[0].toUpperCase()+chaine.slice(1);
+}
+
+  function meteo(){
+
+var latitude=sessionStorage.getItem("latitude");
+var longitude=sessionStorage.getItem("longitude");
+
+     fetch('http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=858044516ecf50294db4aec6cc00ed06&lang=fr&units=metric')
+	.then(res=>res.json())
+	.then(resjson=>{
+      const ville=resjson['name'];
+      const degre=resjson['main']['temp'];
+      const description=resjson['weather'][0]['description'];
+      const icon=resjson['weather'][0]['main'];
+
+
+       document.querySelector('#ville').innerHTML=ville;
+       document.querySelector('#description').innerHTML=capital(description);
+        document.querySelector('#degre').innerHTML=Math.round(degre);
+
+        document.querySelector('i.wi').className=weathericon[icon];
+	}
+      
+
+		);
+
+	console.log(resjson);
+
+     
+}
+
+
+	
+         
+
+
--- Js/JavaScript_Index.js
+++ Js/JavaScript_Index.js
@@ -1,6 +1,7 @@
  function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(showPosition);
+
   } else { 
   	P1.style.visibility="visible";
     P1.innerHTML = "Geolocation is not supported by this browser.";
@@ -10,7 +11,8 @@
 function showPosition(position) {
  sessionStorage.setItem("latitude", position.coords.latitude);
  sessionStorage.setItem("longitude", position.coords.longitude);
-   document.location.href="./Acceuil.html";
+document.location.href="./Acceuil.html";
+   
 }
 function SeConnecter()
 {
@@ -18,22 +20,46 @@ function SeConnecter()
 var PassWord=document.getElementById("password").value;
  var P1=document.getElementById("p1");
 
-if(login=="Hanane" && PassWord=="0903")
+
+if(login=="" && PassWord=="")
+{
+      
+   P1.style.visibility="visible";
+   P1.innerHTML=" Merci de Remplir les Champs!";
+       
+}
+else if(login=="" && PassWord !=""){
+
+   P1.style.visibility="visible";
+   P1.innerHTML=" Merci de Remplir le Champ nom utlisateur  ! ";
+    
+}
+else if(login !="" && PassWord ==""){
+
+   P1.style.visibility="visible";
+   P1.innerHTML=" Merci de Remplir le Champ  mot de passe ! ";
+   
+}
+
+else{
+
+  if(login=="Amine" && PassWord=="Amine123")
 {
         // Store UserName
-  
-       sessionStorage.setItem("UserName", "Hanane");
+       sessionStorage.setItem("UserName", "Amine");
         getLocation();
-
-       
-
+        
 }
 else{ 
-   document.location.href="#"; 
+    
    P1.style.visibility="visible";
    P1.innerHTML="le mot de passe ou nom utlisateur incorrect !";
+  
+}
 
 }
+
+
 
 
 }
